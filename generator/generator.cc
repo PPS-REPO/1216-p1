adb24bbeab368e749b73abf2e9b06fffe26045c7
이성원 <6123@heemang.caees.kr>
2025-05-23 18:56:25 +0900

    Update generator.cc

--- generator/generator.cc
+++ generator/generator.cc
@@ -1,20 +1,33 @@
 #include "testlib.h"
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char** argv) {
-    int al, ar, bl, br, a, b;
-    
+string random_string_with_pattern(const string& pattern, int maxlen) {
+    string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string s = pattern;
+
+    while ((int)s.size() < maxlen && rnd.next(2)) {
+        s += charset[rnd.next((int)charset.size())];
+    }
+
+    shuffle(s.begin(), s.end());
+    s = s.substr(0, rnd.next(1, min(maxlen, (int)s.size())));
+
+    if (s.find("Sumin") == string::npos && s.find("Minsu") == string::npos) {
+        int insert_pos = rnd.next(0, (int)s.size());
+        s.insert(insert_pos, rnd.next(2) ? "Sumin" : "Minsu");
+
+        if ((int)s.size() > maxlen)
+            s = s.substr(0, maxlen);
+    }
+
+    return s;
+}
+
+int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
 
-    al = atoi(argv[1]);
-    ar = atoi(argv[2]);
-    bl = atoi(argv[3]);
-    br = atoi(argv[4]);
-
-    a = rnd.next(al, ar);
-    b = rnd.next(bl, br);
-
-    printf("%d %d\n", a, b);
-    return 0;
+    const int maxlen = 9;
+    string s = random_string_with_pattern(rnd.next(2) ? "Sumin" : "Minsu", maxlen);
+    println(s);
 }
