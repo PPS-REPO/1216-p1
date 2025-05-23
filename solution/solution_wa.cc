4b2d3b1ea366d4cc51f4714d615f888c93bb12a8
이성원 <6123@heemang.caees.kr>
2025-05-23 18:50:03 +0900

    Update solution_wa.cc

--- solution/solution_wa.cc
+++ solution/solution_wa.cc
@@ -1,8 +1,25 @@
-#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+int counting(const string& S, const string& sub) {
+    int ret = 0;
+
+    for (size_t i = 0; i + sub.size() <= S.size(); i++) {
+        if (S.substr(i,sub.size())==sub){
+            ret++;
+        }
+    }
+    
+    return ret;
+}
 
 int main() {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d\n", a + b + 1);
-    return 0;
+    string S;
+    cin >> S;
+    if (counting(S,"Sumin")<counting(S,"Minsu")) {
+        cout << "Sumin";
+    } else {
+        cout << "Minsu";
+    }
 }
