682d8d99e3bacf8d98505beee564a22d5aa8bf74
이성원 <6123@heemang.caees.kr>
2025-05-23 18:54:42 +0900

    Update validator.cc

--- validator/validator.cc
+++ validator/validator.cc
@@ -2,7 +2,7 @@
 using namespace std;
 int main(int argc, char** argv) {
     registerValidation(argc, argv);
-    string S = inf.readToken("[!-~]{1,9}", "S");
+    string S = inf.readToken("[!-~]{1,100}", "S");
     inf.readEoln();
     inf.readEof();
 }
