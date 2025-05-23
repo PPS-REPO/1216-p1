f867a6932568b44cf98e84c25a78c1a9246cdc76
이성원 <6123@heemang.caees.kr>
2025-05-23 18:54:54 +0900

    Update validator_without_testlib.py

--- validator/validator_without_testlib.py
+++ validator/validator_without_testlib.py
@@ -2,6 +2,6 @@ import sys
 inp = sys.stdin.read()
 assert inp.endswith('\n'), "Input must end with a newline"
 s = inp.rstrip('\n')
-assert 1 <= len(s) <= 9, "String length must be between 1 and 9"
+assert 1 <= len(s) <= 100, "String length must be between 1 and 9"
 for c in s:
     assert 33 <= ord(c) <= 126, f"Invalid character: {repr(c)}"
